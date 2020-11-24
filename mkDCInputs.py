b9824d318053eafb16a8b71a00983cf4287c5059
Giacomo Boldrini <gboldrin@cern.ch>
2020-11-24 16:00:52 +0100

    Redefined cfg reader as to allow same process, different ops

--- mkDCInputs.py
+++ mkDCInputs.py
@@ -53,12 +53,7 @@ def makeExec(model, process, config, outdir):
     }
 
     variables = config.getlist("variables", "treenames")
-    ops = config.getlist("eft", "operators")
-    ops = [i[1:-1].split(":") for i in ops]
-    ops = [list(map(str, sublist)) for sublist in ops]
-    samples = config.getlist("general", "sample")
-
-    ops = ops[samples.index(process)]
+    ops = process.split("_")[1:]
 
     mod = modeltot2w[model]
 
@@ -315,11 +310,12 @@ def retireve_samples(config):
     folders = config.getlist(section, "folder")
     #ops = config.getlist("eft", "operators")
     ops_ = redemensionOpinput(config)
+    sample_headers = [i + "_" + "_".join(op for op in k) for i,k in zip(sample,ops_)]
 
-    file_dict = dict.fromkeys(sample)
+    file_dict = dict.fromkeys(sample_headers)
 
-    for s,ops in zip(sample, ops_):
-        file_dict[s] = {}
+    for sh, s,ops in zip(sample_headers, sample, ops_):
+        file_dict[sh] = {}
 
         int_ = len(ops) > 1
 
@@ -328,23 +324,23 @@ def retireve_samples(config):
 
         #scan simple ops
         for op in ops:
-            file_dict[s]
-            file_dict[s]["QU_{}".format(op)] = []
-            file_dict[s]["LI_{}".format(op)] = []
+            file_dict[sh]
+            file_dict[sh]["QU_{}".format(op)] = []
+            file_dict[sh]["LI_{}".format(op)] = []
 
 
             for folder in folders:
                 files = glob(folder + "/*_" + s + "_" + op + "*.root")
                 for file_ in files:
-                    if "QU" in file_: file_dict[s]["QU_{}".format(op)].append(file_)
-                    if "LI" in file_: file_dict[s]["LI_{}".format(op)].append(file_)
+                    if "QU" in file_: file_dict[sh]["QU_{}".format(op)].append(file_)
+                    if "LI" in file_: file_dict[sh]["LI_{}".format(op)].append(file_)
 
                         
         #scan interference if present
         if int_:
             for c1, c2 in zip(comb, comb2):
-                file_dict[s]["IN_{}_{}".format(c1[0], c1[1])] = []
-                file_dict[s]["IN_{}_{}".format(c2[0], c2[1])] = []
+                file_dict[sh]["IN_{}_{}".format(c1[0], c1[1])] = []
+                file_dict[sh]["IN_{}_{}".format(c2[0], c2[1])] = []
 
                 for folder in folders:
                     #either cG_cGtil or cGtil_cG, not both (repetition should be avoided)
@@ -354,23 +350,23 @@ def retireve_samples(config):
                     if len(files1) != 0 and len(files2) == 0: 
                         files = files1
                         c = c1
-                        del file_dict[s]["IN_{}_{}".format(c2[0], c2[1])]
+                        del file_dict[sh]["IN_{}_{}".format(c2[0], c2[1])]
                     elif len(files1) == 0 and len(files2) != 0: 
                         files = files2
                         c = c2
-                        del file_dict[s]["IN_{}_{}".format(c1[0], c1[1])]
+                        del file_dict[sh]["IN_{}_{}".format(c1[0], c1[1])]
                     else:
                         continue
 
                     for file_ in files:
-                        if "IN" in file_: file_dict[s]["IN_{}_{}".format(c[0], c[1])].append(file_)
+                        if "IN" in file_: file_dict[sh]["IN_{}_{}".format(c[0], c[1])].append(file_)
 
 
         sm_fl = []
         for folder in folders:
             sm_fl += glob(folder + "/*" + s + "*SM*.root")
 
-        file_dict[s]["SM"] = sm_fl
+        file_dict[sh]["SM"] = sm_fl
 
     return file_dict
 
@@ -480,13 +476,10 @@ def makeHistos(config, file_dict):
     bins = [int(i) for i in config.getlist("variables", "bins")]
     ranges = convertCfgLists(config.getlist("variables", "xrange"))
     histo_name = config.getlist("variables", "histonames")
-    ops = config.getlist("eft", "operators")
     lumi = float(config.get("general", "lumi"))
 
     dummies = []
     if config.has_option("variables", "makeDummy"): dummies = config.getlist("variables", "makeDummy")
-
-    int_ = len(ops) > 1
 
     if vars_[0] != "*" and len(vars_) != len(bins) or len(vars_) != len(ranges):
         sys.exit("[ERROR] var names and bins/xranges do not match. Ignore or take action ...")
@@ -568,7 +561,7 @@ if __name__ == "__main__":
     if args.verbose: prettyPrintConfig(config, fd)
 
     base_histo = makeHistos(config, fd)
-    
+
     for process in base_histo.keys():
 
         #Saving, discriminating processes
